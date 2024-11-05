7bab53f4532357a42efc33596fcbcdd6baa8acf3
ArtaxJay <okjaceauthor@gmail.com>
2024-11-05 12:33:58 +0100

    Add 1-app.py and 2.app.py and their respective HTML templates

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""ALX Backend project with Flask"""
+
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+from flask_babel import Babel
+
+
+class Config(object):
+    """Babel config"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Checks if locale lang is supported and match if it does."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """Configs the route for the home page"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
