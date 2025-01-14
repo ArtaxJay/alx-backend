bbe4eb7a1e714b3c3623b0c403d0259ae753ece6
Ola-Oluwajuwon <okayode986@stu.ui.edu.ng>
2025-01-14 11:22:52 +0100

    Fix: knowingly introduced bug into 100-lfu_cache.py in order to debug the program.

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""
+""" 
 Module for LFUCache
 """
 from base_caching import BaseCaching
@@ -7,75 +7,69 @@ from base_caching import BaseCaching
 
 class LFUCache(BaseCaching):
     """
-    LFUCache is a caching system that follows the LFU (Least Frequently Used)
-    algorithm. If there are ties in frequency, it uses LRU to decide which item
-    to discard.
+    LFUCache defines a caching system with LFU eviction algorithm.
+    If multiple items have the same frequency, LRU is used to decide which one to discard.
     """
 
     def __init__(self):
         """
-        Initialize the cache.
+        Initialize the class with the parent's init method.
         """
         super().__init__()
-        self.frequency = {}  # Track frequency of access for each key
-        self.usage_order = []  # Track order of usage for LRU handling
+        self.usage = []  # List to track usage order for LRU when frequencies are tied
+        self.frequency = {}  # Dictionary to track frequency of each cache key
 
     def put(self, key, item):
         """
-        Add an item to the cache.
-        If the number of items exceeds MAX_ITEMS, discard the least frequently
-        used item (LFU). In case of ties, the LRU item is discarded.
+        Cache a key-value pair, applying the LFU eviction algorithm.
         """
         if key is None or item is None:
-            return
+            return  # Don't do anything if key or item is None
+        
+        # If the cache is full and the key is not already in cache
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+            # Find the least frequently used items
+            lfu = min(self.frequency.values())
+            lfu_keys = [k for k, v in self.frequency.items() if v == lfu]
+            
+            # If there are ties, use LRU to determine which item to discard
+            if len(lfu_keys) > 1:
+                lru_lfu = {k: self.usage.index(k) for k in lfu_keys}
+                discard = min(lru_lfu, key=lru_lfu.get)
+            else:
+                discard = lfu_keys[0]
 
+            # Print and discard the least used item
+            print(f"DISCARD: {discard}")
+            del self.cache_data[discard]
+            del self.frequency[discard]
+            del self.usage[self.usage.index(discard)]
+        
+        # Update frequency and usage for the current key
         if key in self.cache_data:
             # Update item and frequency
             self.cache_data[key] = item
             self.frequency[key] += 1
-            # Move key to the end to mark it as recently used
-            self.usage_order.remove(key)
-            self.usage_order.append(key)
         else:
             # Add new item
             self.cache_data[key] = item
             self.frequency[key] = 1
-            self.usage_order.append(key)
-
-            # Check if we need to discard an item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                # Find the least frequently used item(s)
-                min_freq = min(self.frequency.values())
-                least_frequent = [
-                    k for k, v in self.frequency.items() if v == min_freq
-                ]
-
-                # If there's a tie, use LRU to decide which to discard
-                if len(least_frequent) > 1:
-                    for k in self.usage_order:
-                        if k in least_frequent:
-                            key_to_discard = k
-                            break
-                else:
-                    key_to_discard = least_frequent[0]
-
-                # Remove the chosen key
-                del self.cache_data[key_to_discard]
-                del self.frequency[key_to_discard]
-                self.usage_order.remove(key_to_discard)
-                print(f"DISCARD: {key_to_discard}")
+        
+        # Update the usage order to mark this key as recently used
+        if key in self.usage:
+            self.usage.remove(key)  # Remove the key to reinsert it at the end
+        self.usage.append(key)
 
     def get(self, key):
         """
-        Retrieve an item by key from the cache.
-        If key is None or the key doesn't exist, return None.
+        Return the value linked to a given key, or None if not found.
         """
         if key is None or key not in self.cache_data:
-            return None
-
-        # Update frequency and usage order
+            return None  # If key is not found, return None
+        
+        # Update the frequency and usage order
         self.frequency[key] += 1
-        self.usage_order.remove(key)
-        self.usage_order.append(key)
-
+        self.usage.remove(key)
+        self.usage.append(key)
+        
         return self.cache_data[key]
